88910a8f62e275c99a4cec3f7dce6f14306197cd
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2022-11-22 03:25:05 -0800

    I added the main.h header file

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_c(va_list ar_list);
+int (*get_func(char s))(va_list ar_list);
+int print_s(va_list ar_list);
+int print_d(va_list ar_list);
+int print_i(va_list ar_numlist);
+int print_b(va_list binary_list);
+int print_u(va_list ar_list);
+int print_o(va_list ar_list);
+int print_x(va_list ar_list);
+int print_X(va_list ar_list);
+
+#endif
